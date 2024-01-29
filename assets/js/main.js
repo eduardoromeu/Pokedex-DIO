2ad1a6d1bc3bc7b65ff906a458e7632e6ba74a37
eduardoromeu <eduardoromeueberts@gmail.com>
2024-01-29 15:46:43 -0300

    Added basic pokemon info to modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,12 @@ const loadMoreButton = document.getElementById("loadMoreButton");
 const pokemonModalContainer = document.getElementById("pokemonModalContainer");
 const pokemonModal = document.getElementById("pokemonModal");
 const closeModalBtn = document.getElementById("closeModalBtn");
+const modalTitle = document.getElementById("pokemonName");
+const modalNumber = document.getElementById("pokemonNumber");
+const modalTypes = document.getElementById("pokemonTypes");
+const modalDetailList = document.getElementById("pokemonDetailList");
+const modalPrevBtn = document.getElementById("prevPokemonBtn");
+const modalNextBtn = document.getElementById("nextPokemonBtn");
 
 const maxRecords = 151;
 const limit = 8;
@@ -16,18 +22,12 @@ closeModalBtn.addEventListener('click', CloseModal);
 
 pokemonModalContainer.addEventListener('click', CloseModal);
 
-// pokemonModalContainer.addEventListener('click', (e) => {
-//     if(pokemonModal.getAttribute("visible") == "true" && e.target.id == "pokemonModalContainer"){
-//         CloseModal();  
-//     } 
-// });
-
 function loadPokemonItems(offset, limit) {
 
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
         <li class="pokemon ${pokemon.type}" id="${pokemon.name}-card">
-            <span class="number">${pokemon.number}</span>
+            <span class="number">#${pokemon.number}</span>
             <span class="name"><a href="#">${pokemon.name}</a></span>
             <div class="detail">
                 <ol class="types">
@@ -69,6 +69,20 @@ function LoadMoreItems(){
 
 function ShowModal(pokemon){
     pokemonModal.setAttribute("visible", "true");
+    
+    modalTitle.innerText = pokemon.name;
+    modalNumber.innerText = pokemon.number;
+
+    let typesHtml = pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('');
+    modalTypes.innerHTML = typesHtml;
+
+    modalPrevBtn.addEventListener('click', () => {
+
+    });
+
+    modalNextBtn.addEventListener('click', () => {
+        
+    });
 }
 
 function CloseModal(e){
